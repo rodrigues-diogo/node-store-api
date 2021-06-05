495fc8a5fea839814e8c3d2c3dace6dd8919499c
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-05 20:34:45 -0300

    Add more routers to create a crud

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
+
 const router = express.Router();
 
 const route = router.get("/", (req, res, next) => {
@@ -11,6 +13,26 @@ const route = router.get("/", (req, res, next) => {
     version: "0.0.1",
   });
 });
+
+const create = router.post("/", (req, res, next) => {
+  res.status(201).send(req.body);
+});
+
+const put = router.put("/:id", (req, res, next) => {
+  const id = req.params.id;
+  res.status(200).send({
+    id: id,
+    item: req.body,
+  });
+});
+
+const del = router.delete("/:id", (req, res, next) => {
+  res.status(200).send(req.body);
+});
+
 app.use("/", route);
+app.use("/products", create);
+app.use("/products", put);
+app.use("/products", del);
 
 module.exports = app;
