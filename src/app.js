73affb9bed6009c7ce61f1fe78471400d0b0a8f6
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-05 21:46:25 -0300

    Remove routes from app.js code and export from routes created

--- src/app.js
+++ src/app.js
@@ -1,38 +1,13 @@
 "use strict";
 
 const express = require("express");
+const indexRoute = require("./routes/index-route");
+const productRoute = require("./routes/products-route");
 
 const app = express();
 app.use(express.json());
 
-const router = express.Router();
-
-const route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1",
-  });
-});
-
-const create = router.post("/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body,
-  });
-});
-
-const del = router.delete("/:id", (req, res, next) => {
-  res.status(200).send(req.body);
-});
-
-app.use("/", route);
-app.use("/products", create);
-app.use("/products", put);
-app.use("/products", del);
+app.use("/", indexRoute);
+app.use("/products", productRoute);
 
 module.exports = app;
