0e23a128da66b10896ec7c9dd4044b0e619d6753
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-07 22:12:08 -0300

    Add this route to the app

--- src/routes/products-route.js
+++ src/routes/products-route.js
@@ -5,6 +5,7 @@ const controller = require("../controllers/products-controller");
 
 const router = express.Router();
 
+router.get("/", controller.get);
 router.post("/", controller.post);
 router.put("/:id", controller.put);
 router.delete("/:id", controller.del);
