4203e0150ff1fc895f1e6809b4f7c7e41bbdf1e0
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-07 22:19:04 -0300

    Add getBySlug controller method to app route

--- src/routes/products-route.js
+++ src/routes/products-route.js
@@ -6,6 +6,7 @@ const controller = require("../controllers/products-controller");
 const router = express.Router();
 
 router.get("/", controller.get);
+router.get("/:slug", controller.getBySlug);
 router.post("/", controller.post);
 router.put("/:id", controller.put);
 router.delete("/:id", controller.del);
