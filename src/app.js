055cce859e7531a8e721e455c24ca0ed1ad8ef8a
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-07 21:18:51 -0300

    Get env variables and use it to connect to mongoDB

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
 "use strict";
 
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zqvrb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/products-route");
 
