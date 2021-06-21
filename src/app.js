6ce3a594398de0eec37810c3a1239b91cae75a14
diogo-rodrigues-silva <diogorodrigues0801@gmail.com>
2021-06-20 22:07:57 -0300

    Add customers and orders routers to app

--- src/app.js
+++ src/app.js
@@ -10,11 +10,15 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const indexRoute = require("./routes/index-route");
 const productRoute = require("./routes/products-route");
+const customerRoute = require("./routes/customers-route");
+const orderRoute = require("./routes/order-route");
 
 const app = express();
 app.use(express.json());
 
 app.use("/", indexRoute);
 app.use("/products", productRoute);
+app.use("/customers", customerRoute);
+app.use("/orders", orderRoute);
 
 module.exports = app;
